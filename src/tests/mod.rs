d96ad62d5fa78bf5b7432bc82f9eb7d2a240393a
Benjamin Navarro <navarro.benjamin13@gmail.com>
2018-12-06 08:35:43 +0100

    ForceMotion vectors test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use std::f64;
     use sva;
 
     static TOL: f64 = 0.00001;
@@ -74,5 +75,77 @@ mod tests {
 
         // zero
         assert_eq!(sva::MotionVector::zero().vector(), sva::Vec6::zeros());
+    }
+
+    #[test]
+    fn force_vector_test() {
+        let n = sva::Vec3::new_random();
+        let f = sva::Vec3::new_random();
+
+        let vec = sva::ForceVector::from_vectors(n, f);
+        let m = vec.vector();
+
+        // couple
+        assert_eq!(n, vec.couple);
+
+        // force
+        assert_eq!(f, vec.force);
+
+        // vector
+        assert_eq!(
+            m,
+            sva::Vec6::new(
+                vec.couple[0],
+                vec.couple[1],
+                vec.couple[2],
+                vec.force[0],
+                vec.force[1],
+                vec.force[2]
+            )
+        );
+
+        // alpha*F
+        assert_eq!((5. * vec).vector(), 5. * m);
+
+        // F*alpha
+        assert_eq!((vec * 5.).vector(), 5. * m);
+
+        // F/alpha
+        assert_eq!((vec / 5.).vector(), m / 5.);
+
+        // -F
+        assert_eq!((-vec).vector(), -m);
+
+        let n2 = sva::Vec3::new_random();
+        let f2 = sva::Vec3::new_random();
+        let vec2 = sva::ForceVector::from_vectors(n2, f2);
+        let m2 = vec2.vector();
+
+        // F + F
+        assert_eq!((vec + vec2).vector(), m + m2);
+
+        // F - F
+        assert_eq!((vec - vec2).vector(), m - m2);
+
+        // F += F
+        let mut vec_pluseq = vec;
+        vec_pluseq += vec2;
+        assert_eq!(vec_pluseq, vec + vec2);
+
+        // F -= F
+        let mut vec_minuseq = vec;
+        vec_minuseq -= vec2;
+        assert_eq!(vec_minuseq, vec - vec2);
+
+        // ==
+        assert_eq!(vec, vec);
+        assert_ne!(vec, -vec);
+
+        // !=
+        assert!(vec != (-vec));
+        assert!(!(vec != vec));
+
+        // zero
+        assert_eq!(sva::ForceVector::zero().vector(), sva::Vec6::zeros());
     }
 }
